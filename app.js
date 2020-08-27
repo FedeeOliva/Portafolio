e6d5edd14f5128d619694710302ae577d7ff0876
Fedee Oliva <fedeolivalr@gmail.com>
2020-08-27 15:29:43 -0300

    smooth scroll

--- app.js
+++ app.js
@@ -29,3 +29,17 @@
 	}
 	window.addEventListener('scroll', handleScroll);
 })();
+
+(()=>{
+	  let buttons = document.querySelectorAll('a');
+	  buttons.forEach((button) => {
+	    button.addEventListener('click', (e) => {
+	      e.preventDefault();
+	      let target = e.currentTarget.getAttribute('href');
+	      
+	      document.querySelector(target).scrollIntoView({
+	        behavior: 'smooth'
+	      });
+	    });
+	  });
+})();
